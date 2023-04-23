df6ac12d06a1afb531a57cf4b6421471af68cd35
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-04-23 18:36:50 +0300

    Adding

---
+++ src/components/statistics/Section.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { FeedbackOptions } from "./FeedbackOptions";
+
+export class Section extends Component
+{
+    state = { title: "Please leave feedback" };
+
+    render()
+    {
+        const { title } = this.state;
+
+        return (
+            <div>
+                <h1 style={{ paddingTop: 40, textAlign: "center" }}>{title}</h1>
+                <FeedbackOptions options={0} onLeaveFeedback={0}/>
+            </div>
+        );        
+    }
+}
+Section.propTypes =
+{
+    title: PropTypes.string.isRequired
+};
